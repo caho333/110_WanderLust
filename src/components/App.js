5494a623f7cf96347ef4068cfa0095d6b481e8d1
Ahan Mukhopadhyay <ahan.m13@gmail.com>
2018-05-01 12:54:50 -0700

    Structured app to react-starter-kit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+}
+
+export default App;
